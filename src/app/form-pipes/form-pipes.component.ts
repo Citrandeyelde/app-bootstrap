f174ca702a23787c3bab5bbb20551b275d70d646
Citrandeyelde <adrianrojas100@gmail.com>
2024-08-01 21:41:31 -0500

    modulando la fecha

--- src/app/form-pipes/form-pipes.component.ts
+++ src/app/form-pipes/form-pipes.component.ts
@@ -1,12 +1,33 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-pipes',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './form-pipes.component.html',
   styleUrl: './form-pipes.component.css'
 })
 export class FormPipesComponent {
-
+  mostrarResultado=0
+  fechaActual= new Date()
+  alumnos=[
+    {nombre:"Ugaz"},
+    {nombre:"Gamarra"},
+    {nombre:"Rojas"},
+    {nombre:"Carbajo"},
+    {nombre:"Polleri"}
+  ]
+  curso=[
+    {nombre:"Desarrollo Web", profesor:"Damaso Lopez",precio:2500},
+    {nombre:"Proyecto Certificador", profesor:"Marco Villa",precio:1500},
+    {nombre:"Ingles Tecnico", profesor:"Maria Chavez",precio:2500},
+    {nombre:"Servicios Web 1", profesor:"Silfrido Alva",precio:500}
+  ]
+  mostrarAlumnos(){
+    this.mostrarResultado =1
+  }
+  mostrarCursos(){
+    this.mostrarResultado =0
+  }
 }
